d543ac7cd309b91849dc39a52c1cd6387929109a
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-11-28 16:14:09 -0500

    Fixed bug with missing field

--- src/createCurrencyEmbed.ts
+++ src/createCurrencyEmbed.ts
@@ -39,10 +39,11 @@ export async function createCurrencyEmbed(guild: Guild, copperCount: number) {
     .setDescription(
       "The amount of generic non useful assets the party has acquired."
     )
-    .addField(`${emojis[0]}<-Platinum->${emojis[0]}`, currency.platinum)
-    .addField(`${emojis[1]}<-Gold->${emojis[1]}`, currency.gold)
-    .addField(`${emojis[2]}<-Silver->${emojis[2]}`, currency.silver)
-    .addField(`${emojis[3]}<-Copper->${emojis[3]}`, currency.copper)
+    .addField(`${emojis[0]}<-Platinum->${emojis[0]}`, currency.platinum, true)
+    .addField(`${emojis[1]}<-Gold->${emojis[1]}`, currency.gold, true)
+    .addField(`${emojis[2]}<-Electrum->${emojis[2]}`, currency.electrum, true)
+    .addField(`${emojis[3]}<-Silver->${emojis[3]}`, currency.silver, true)
+    .addField(`${emojis[4]}<-Copper->${emojis[4]}`, currency.copper, true)
     .setFooter(
       "Created by Drangon#6334",
       "https://cdn.discordapp.com/avatars/255482941876994059/0a525588185d1f03b9eb548b59981abb.png?size=128"
