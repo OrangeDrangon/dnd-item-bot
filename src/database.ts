76f802d9b2732378a5649190ac16c1d718bc3adc
Kyle Rosenberg <kyle@ekrosenberg.com>
2019-12-22 18:35:27 -0500

    Added sleeping and database connection retries to a max of 60 seconds

--- src/database.ts
+++ src/database.ts
@@ -7,13 +7,15 @@ import connect, {
 import { Wallet, WalletEntry, WalletQuery } from "./interfaces/wallet";
 import { generateAndQuery, generateCommaQuery } from "./utils/generateQuery";
 import { logger } from "./utils/logger";
+import { sleep } from "./utils/sleep";
 
 async function initializeDatabase(
   connectionPool: ConnectionPool
-): Promise<void> {
+): Promise<boolean> {
   logger.info("Initializing database");
-  logger.info("Creating wallets channel if it does not exisit.");
-  await connectionPool.query(sql`CREATE TABLE IF NOT EXISTS wallets (
+  try {
+    logger.info("Creating wallets channel if it does not exisit.");
+    await connectionPool.query(sql`CREATE TABLE IF NOT EXISTS wallets (
     "id"            SERIAL PRIMARY KEY,
     "guildId"       VARCHAR(25) NOT NULL,
     "channelId"     VARCHAR(25) NOT NULL,
@@ -26,12 +28,17 @@ async function initializeDatabase(
     "silver"        INT NOT NULL,
     "copper"        INT NOT NULL
   );`);
-  logger.info("Successfully created wallets table.");
-  logger.info("Creating guildId index for faster searches.");
-  await connectionPool.query(
-    sql`CREATE INDEX IF NOT EXISTS "guildIdIndex" ON wallets ("guildId");`
-  );
-  logger.info("Successfully created guildId index.");
+    logger.info("Successfully created wallets table.");
+    logger.info("Creating guildId index for faster searches.");
+    await connectionPool.query(
+      sql`CREATE INDEX IF NOT EXISTS "guildIdIndex" ON wallets ("guildId");`
+    );
+    logger.info("Successfully created guildId index.");
+    return true;
+  } catch (error) {
+    logger.error("Error initializing database" + error != null ? error : "");
+    return false;
+  }
 }
 
 export interface Database {
@@ -51,7 +58,12 @@ export async function createDatabase({
 }): Promise<Database> {
   const connectionPool = connect(connectionConfig, connectionOptions);
 
-  await initializeDatabase(connectionPool);
+  let timeout = 1000;
+  while (!(await initializeDatabase(connectionPool))) {
+    logger.info(`Retrying in ${timeout / 1000} seconds.`);
+    await sleep(timeout);
+    timeout = timeout < 60000 ? timeout * 2 : timeout;
+  }
 
   return {
     rawQuery: async (query: string): Promise<unknown[]> => {
